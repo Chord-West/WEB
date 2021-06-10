7fa32eadcca7611e25500575cd776eec0c700e58
unknown <gustj2005@naver.com>
2021-06-11 00:02:36 +0900

    회원가입 페이지 및 코드스플릿팅 설정

--- ReactStudy/sleact/alecture/layouts/App.tsx
+++ ReactStudy/sleact/alecture/layouts/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import loadable from "@loadable/component";
 import {Switch,Route,Redirect} from "react-router";
-import  Login from '@pages/LogIn';
-import  SignUp from '@pages/SignUp';
 
 
+const LogIn = loadable(()=> import('@pages/Login')); // 코드 스플릿팅을 위한 설정
+const SignUp = loadable(()=> import('@pages/SignUp'));
 const App = () => {
     return (
         <Switch>
             <Redirect exact path="/" to="/login"/>
-            <Route path="/login" component={Login}/>
+            <Route path="/login" component={LogIn}/>
             <Route path="/signup" component={SignUp}/>
         </Switch>
     );

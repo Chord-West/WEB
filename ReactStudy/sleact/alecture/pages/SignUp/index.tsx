15b3d70e6f458dea42b6fc0e19237127ae98993a
unknown <gustj2005@naver.com>
2021-06-17 00:19:29 +0900

    study custom hooks, useCallback

--- ReactStudy/sleact/alecture/pages/SignUp/index.tsx
+++ ReactStudy/sleact/alecture/pages/SignUp/index.tsx
@@ -1,17 +1,32 @@
 import React, {useCallback, useState} from "react";
-import {Header,Form,Label,Input,LinkContainer,Button} from "./styles";
+import useInput from "@hooks/useInput";
+import {Header,Form,Label,Input,LinkContainer,Button,Error} from "./styles";
 
 
 const SignUp = () =>{
-    const [email] = useState('');
-    const[nickname] = useState('');
-    const[password] = useState('');
-    const[passwordCheck] = useState('');
-    const onChangeEmail = useCallback(()=>{},[]);
-    const onChangeNickname = useCallback(()=>{},[]);
-    const onChangePassword = useCallback(()=>{},[]);
-    const onChangePasswordCheck = useCallback(()=>{},[]);
-    const onSubmit = useCallback(()=>{},[]);
+    const [email ,onChangeEmail] = useInput('');
+    const[nickname, onChangeNickname] = useInput('');
+    const[password, ,setPassword] = useInput('');
+    const[passwordCheck,,setPasswordCheck] = useInput('');
+    const [mismatchError,setMismatchError] = useState(false);
+
+    const onChangePassword = useCallback((e)=>{
+        setPassword(e.target.value);
+        setMismatchError(e.target.value!==passwordCheck);
+    },[passwordCheck]);
+    const onChangePasswordCheck = useCallback((e)=>{
+        setPasswordCheck(e.target.value);
+        setMismatchError(e.target.value!==password);
+    },[password]);
+
+    const onSubmit = useCallback((e)=>{ // useCallback 은 deps에 넣어놧떤 값을 기억하라는 것
+        // useCallback으로 감싸지 않으면 다시 재생성됨 => 리렌더링이 많이 일어남, 디버깅하기 어려워짐
+        e.preventDefault();
+        console.log(email,nickname,password,passwordCheck);
+        if (!mismatchError){
+            console.log('서버로 회원가입하기');
+        }
+    },[email,nickname,password,passwordCheck]);
     return (
         <div id="container">
             <Header>Sleact</Header>
@@ -45,8 +60,8 @@ const SignUp = () =>{
                             onChange={onChangePasswordCheck}
                         />
                     </div>
-                    {/*{mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}*/}
-                    {/*{!nickname && <Error>닉네임을 입력해주세요.</Error>}*/}
+                    {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+                    {!nickname && <Error>닉네임을 입력해주세요.</Error>}
                     {/*{signUpError && <Error>{signUpError}</Error>}*/}
                     {/*{signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}*/}
                 </Label>
